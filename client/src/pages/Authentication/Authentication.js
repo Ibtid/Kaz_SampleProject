58d1b7e7b059f99df2b277b96ab899dcb4b363f6
Ibtid <ibtid011@example.com>
2022-05-09 18:14:37 +0600

    catching inputs through useState

--- client/src/pages/Authentication/Authentication.js
+++ client/src/pages/Authentication/Authentication.js
@@ -2,49 +2,81 @@ import React, { useState } from 'react';
 import './Authentication.css';
 
 const Authentication = () => {
-  const [login, setLogin] = useState(false);
+  const [hasAccount, setHasAccount] = useState(false);
+
+  const [formData, setFormData] = useState({
+    userName: '',
+    email: '',
+    password: '',
+  });
+
+  const onChangeFormData = (e) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  };
+
+  const resgister = () => {
+    console.log(formData);
+  };
+
+  const login = () => {
+    console.log(formData);
+  };
+
   return (
     <div className='authentication'>
       <div className='authentication__container'>
         <div className='authentication__logo'>SampleMart</div>
-        {!login && (
+        {!hasAccount && (
           <div className='authentication__inputContainer'>
             <div className='authentication__inputLabel'>Username</div>
             <input
+              name='userName'
               type='text'
               placeholder='Your name'
               className='authentication__input'
+              value={formData.userName}
+              onChange={onChangeFormData}
             />
           </div>
         )}
         <div className='authentication__inputContainer'>
           <div className='authentication__inputLabel'>Email</div>
           <input
+            name='email'
             type='text'
             placeholder='@gmail.com'
             className='authentication__input'
+            value={formData.email}
+            onChange={onChangeFormData}
           />
         </div>
         <div className='authentication__inputContainer'>
           <div className='authentication__inputLabel'>Password</div>
           <input
+            name='password'
             type='password'
             placeholder='Atleast 6 characters'
             className='authentication__input'
+            value={formData.password}
+            onChange={onChangeFormData}
           />
         </div>
-        {login ? (
-          <div className='authentication__button'>Login</div>
+        {hasAccount ? (
+          <div className='authentication__button' onClick={login}>
+            Login
+          </div>
         ) : (
-          <div className='authentication__button'>Register</div>
+          <div className='authentication__button' onClick={resgister}>
+            Register
+          </div>
         )}
-        {!login ? (
+        {!hasAccount ? (
           <div className='authentication__smallText'>
             Already have an account?{' '}
             <span
               className='highlightedText'
               onClick={() => {
-                setLogin(true);
+                setHasAccount(true);
               }}>
               Login
             </span>
@@ -55,7 +87,7 @@ const Authentication = () => {
             <span
               className='highlightedText'
               onClick={() => {
-                setLogin(false);
+                setHasAccount(false);
               }}>
               Register
             </span>
